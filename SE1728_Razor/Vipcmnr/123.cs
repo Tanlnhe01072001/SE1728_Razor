3e7fe282acaabbf8a948f33fee4e238991bc7cbe
Nhat Tan <tanln01072001@gmail.com>
2024-06-17 01:49:19 +0700

    update nhanh 2

---
+++ SE1728_Razor/Vipcmnr/123.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SE1728_Razor.Vipcmnr
+{
+    public class _123 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
